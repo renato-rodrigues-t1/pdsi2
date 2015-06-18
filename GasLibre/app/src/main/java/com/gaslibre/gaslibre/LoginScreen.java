fe5aa94e11d309423ec209bdcb327cdaaaf38ecf
renatosistemas <renato.rsufu@gmail.com>
2015-06-17 22:44:39 -0300

    ligação de componentes
    ligação de componentes

--- GasLibre/app/src/main/java/com/gaslibre/gaslibre/LoginScreen.java
+++ GasLibre/app/src/main/java/com/gaslibre/gaslibre/LoginScreen.java
@@ -10,12 +10,22 @@ import Model.User;
 import android.util.Log;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by renato on 6/15/15.
  */
-public class LoginScreen extends Activity {
+public class LoginScreen extends Activity implements View.OnClickListener {
     private UserDAO userDAO= new UserDAO(this);
+    private TextView textoUsuauio;
+    private TextView textoSenha;
+    private EditText editUser;
+    private EditText editSenha;
+    private Button buttonLogin;
+    private Button buttonRegistrar;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,8 +34,7 @@ public class LoginScreen extends Activity {
         DBManager dbManager= new DBManager(this);//inicializa as tabelas se naum existem.
 
         inicializaComponentes();
-        test();
-
+        //test();
         //String email= textfild.getString
         //String senha=
         //userController.autenticaUsuario(email, senha);
@@ -36,15 +45,23 @@ public class LoginScreen extends Activity {
 
     }
 
-    private void inicializaComponentes(){
-        //Texfild email= findById(R.id.email...)
-        // ||               senha...
+    private void inicializaComponentes() {
+
+        textoUsuauio= (TextView)findViewById(R.id.textoUsuario);
+        textoSenha= (TextView)findViewById(R.id.textSenha);
+        editUser= (EditText) findViewById(R.id.editUsuario);
+        editSenha= (EditText) findViewById(R.id.editSenha);
+        buttonLogin= (Button) findViewById(R.id.botaoLogin);
+        buttonRegistrar= (Button) findViewById(R.id.botaoRegistrar);
 
     }
 
     public Context getContext(){
         return this.getApplicationContext();
     }
+
+    
+
     private void test(){
         User userTemp= new User("Bruno Ernandes", "higor.rsufu@gmail.com", "1234");
         Log.v("USER>>>", userTemp.getName()+"- "+userTemp.getEmail()+"- "+userTemp.getSenha());
